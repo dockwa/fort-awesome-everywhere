b1d5804a06c7e9068d3ed4c1de779f387a3c35a4
Stuart Yamartino <stuart@yamartino.com>
2016-09-14 00:29:48 -0400

    Adding android support

---
+++ script.js
@@ -0,0 +1,53 @@
+$(function(){
+
+  $('#ios-button').click(function(){
+    var kitID = $('input[name=ios_kit_id]').val();
+    getData(kitID, exportJSON);
+  });
+
+  $('#andriod-button').click(function(){
+    var kitID = $('input[name=andriod_kit_id]').val();
+    getData(kitID, exportXML);
+  });
+
+});
+
+var getData = function(kitID, exportFunction){
+  $.get("https://use.fortawesome.com/kits/" + kitID + "/" + kitID + ".css", function( data ) {
+    var prefix = data.match(/}.([a-zA-Z0-9-_]+){/);
+    if(prefix == null){
+      alert('Your data is not in the correct format.');
+      return;
+    }
+    var result = [];
+    var regex = new RegExp("." + prefix[1] + "-([a-zA-Z\-]+):before{content:'\\\\([a-zA-Z0-9]+)'}", "gi");
+    var test = regex.exec(data);
+    result.push(test);
+    while (test != null) {
+      test = regex.exec(data);
+      if(test != null){
+        result.push(test);
+      }
+    }
+    exportFunction(result);
+  })
+  .fail(function() {
+    alert('Your kit could not be found');
+  });
+}
+
+var exportJSON = function(result){
+  var json = {};
+  for(var i = 0, l = result.length; i < l; i++){
+    json[result[i][1]] = '\\u' + result[i][2];
+  }
+  $('#ios-output').val(JSON.stringify(json));
+}
+
+var exportXML = function(result){
+  var json = {};
+  for(var i = 0, l = result.length; i < l; i++){
+    json[result[i][1]] = '\\u' + result[i][2];
+  }
+  $('#andriod-output').val(JSON.stringify(json));
+}
